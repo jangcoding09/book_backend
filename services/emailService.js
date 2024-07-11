7b6994268c104369c8b8d831a5319af69aac784f
jangco97 <qkrnslehd@naver.com>
2024-07-11 15:05:16 +0900

    이메일 서비스

---
+++ services/emailService.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+
+// Mailtrap SMTP 설정
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "jgw970907@gmail.com",
+    pass: "atsnhnrxlxeurqwa",
+  },
+});
+
+// 이메일 보내기 함수 예시
+const sendEmail = async (toEmail, subject, text) => {
+  try {
+    const info = await transporter.sendMail({
+      from: "jgw970907@gmail.com",
+      to: toEmail,
+      subject: subject,
+      text: text,
+    });
+    console.log("Email sent: " + info.response);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
+
+module.exports = { sendEmail };
