c915b0886a8f2a8f73eca793db9e98f6ae7b36ea
jangco97 <qkrnslehd@naver.com>
2024-07-16 21:06:42 +0900

    vercel.json 교체

--- api/getCount.js
+++
@@ -1,7 +0,0 @@
-const { getCount } = require("../controllers/statisticController");
-const { connectDB } = require("../config/db");
-
-module.exports = async (req, res) => {
-  await connectDB();
-  getCount(req, res);
-};
