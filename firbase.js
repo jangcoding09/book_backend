3c3eafd03cbf9782a448e173323a46e1a693e5a0
jangco97 <qkrnslehd@naver.com>
2024-07-16 21:54:03 +0900

    firebase

--- firbase.js
+++ firbase.js
@@ -1,6 +1,17 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseConfig.json");
-// 환경 변수에서 Firebase 설정 값을 읽어와 초기화합니다.
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
