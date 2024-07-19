d44f64f3535a003fedd1d9abd5cd58e0fff6dfff
jangco97 <qkrnslehd@naver.com>
2024-07-19 17:17:57 +0900

    라우트 추가

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {
   signup,
   login,
-  verifyEmail,
   getUser,
   logout,
   getAccessToken,
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const authenticate = require("../middleware/authMiddleware");
 
-const { getUsers, patchUser } = require("../controllers/usersController");
+const {
+  getUsers,
+  patchUser,
+  deleteUser,
+} = require("../controllers/usersController");
 // const authenticate = require("../middleware/authMiddleware");
 // 유저 삭제는 권한을 role이 admin인 사람만 권한이 있음!!
 // role을 검증하는 과정이 필요함
-
+router.delete("/delete/:id", authenticate, deleteUser);
 router.get("/", getUsers);
-router.patch("/update", patchUser);
+router.patch("/update", authenticate, patchUser);
 module.exports = router;
