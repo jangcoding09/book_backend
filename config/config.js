0effd306ce0d30452e05d56984676289e18b19f4
jgw970907 <qkrnslehd@naver.com>
2025-01-20 13:34:06 +0900

    config변경

--- config/config.js
+++ config/config.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
 const { URL } = require("url");
 const pg = require("pg");
-const databaseUrl = new URL(process.env.POSTGRES_URL);
 
 module.exports = {
   development: {
-    username: databaseUrl.username,
-    password: databaseUrl.password,
-    database: databaseUrl.pathname.split("/")[1],
-    host: databaseUrl.hostname,
-    port: databaseUrl.port,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT || 5432,
     schema: "public", // 스키마 지정
     dialect: "postgres",
     dialectModule: require("pg"), // pg 모듈
